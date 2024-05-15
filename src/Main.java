326d2c42b09eb0af0cbe080ab8dd3febeb256ed4
Atharv Joshi <joshi.atharv1@northeastern.edu>
2024-05-14 21:36:28 -0400

    Adding Custom Email Feature

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,50 @@ import net.glxn.qrgen.image.ImageType;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.awt.Desktop;
+import java.net.URI;
+public class Main {
 
-public class Main{
-    public static void main(String[] args){
-        //To handle I/O Exceptions
-        try{
-            File file=new File("abc.png"); // To make image in current directory or to create images on Desktop
-            //You can find the path to desktop from command line
-            //File file=new File("/c/Users/Atharv Joshi/Desktop/abc.png");
+    public static void main(String[] args) {
+        String emailAddress = "example@example.com";
+        String messageBody = "Hi , \n" +
+                "\n" +
+                "This is Atharv, I am currently pursuing my masters at Northeastern university in information systems. I am actively looking for internship opportunities and I believe that I have the right mindset and potential to learn and grow in my career. \n" +
+                "\n" +
+                "I would greatly appreciate if we could get on a call and discuss any possible opportunities in your company or in your circle.\n" +
+                "\n" +
+                "My tech skills:\n" +
+                "Java - JDBC drivers, Java servlet (JSP), APIs\n" +
+                "Database - SQL, MySQL, MongoDB, DB2\n" +
+                "DevOps -  CI/CD pipelines, Shell scripting, Terraform, Packer\n" +
+                "Testing - Manual testing, DevOps, JavaScript (Zest, coffee)\n" +
+                "JavaScript - Node.js, React.js, Next.js\n" +
+                "Cloud - Azure, Google cloud, AWS\n" +
+                "Networking - Gateway, Firewall, Security\n" +
+                "\n" +
+                "Best Regards\n" +
+                "Atharv\n"+
+                "Link to My Resume Below\n"+
+                "https://drive.google.com/file/d/11dbSlIdAsfSJCdmNR89-Nu3KfCGJ8jgI/view";
 
-            String content="This is my content";
-            ByteArrayOutputStream out= QRCode.from(content).to(ImageType.PNG).stream();//  returns the image data as a byte array stream.
-            //Out has our content now we want to write that content into the file
-            FileOutputStream fos=new FileOutputStream(file);// Where to store the qr code created
+        // Encode the message body, replacing spaces with %20 and line breaks with %0D%0A
+        String encodedMessageBody = messageBody.replaceAll(" ", "%20").replaceAll("\n", "%0D%0A");
+
+        // Construct the mailto URI
+        String uriString = "mailto:" + emailAddress + "?body=" + encodedMessageBody;
+
+        try {
+            File file = new File("mylink2.png"); // To make image in current directory or to create images on Desktop
+            ByteArrayOutputStream out = QRCode.from(uriString).to(ImageType.PNG).stream(); // returns the image data as a byte array stream.
+            // Out has our content now we want to write that content into the file
+            FileOutputStream fos = new FileOutputStream(file); // Where to store the qr code created
             fos.write(out.toByteArray());
             fos.close();
             System.out.println("QR Code is Generated");
+            Desktop desktop = Desktop.getDesktop();
+            desktop.mail(new URI(uriString)); // Pass uriString instead of qrData
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch(Exception e){
-         e.printStackTrace();
-        }
-        }
-        }
+    }
+}
